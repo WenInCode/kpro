351f495718c5e91f26b6dfb0d28fc5ff1bf502ed
wenincode <twendlan@ualberta.ca>
2017-03-14 13:25:23 -0600

    Moar Progress

--- bin/sad.js
+++ bin/sad.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
 
-const args = require('args');
-const cp = require('child_process');
-const _string = require('lodash/string');
-const parsePSOutput = require('../lib/parse-ps-output');
+const args = require('args')
+const chalk = require('chalk')
+const cp = require('child_process')
+const _string = require('lodash/string')
+
+const parsePSOutput = require('../lib/parse-ps-output')
+const handleMatchedProcesses = require('../lib/handle-processes')
 
 args
   .option('no-confirm', 'Kill matched processes without confirmation', false)
-  .option('no-fun', 'Strictly have no fun', false)
-  .command('echo', 'Echo back some content', ['e']);
+  .command('echo', 'Echo back some content', ['e'])
 
-const flags = args.parse(process.argv, { value: '<search values>' });
-const value = args.sub[0];
+const flags = args.parse(process.argv, { value: '<search values>' })
+const value = args.sub[0]
 
 if (value === "" || value === undefined) {
-  args.showHelp();
-  return;
+  args.showHelp()
+  return
 }
 
-cp.exec(`ps aux | grep ${value}`, function(err, stdout, stderr) {
-  console.log('ERR:', err);
-  console.log('STDOUT:', stdout);
-  console.log('STDERR:', stderr);
-
-  console.log(parsePSOutput(stdout));
+cp.exec(`ps | grep ${value}`, (err, stdout, stderr) => {
+  if (err) {
+    console.log(chalk.red(err))
+  } else {
+    handleMatchedProcesses(parsePSOutput(stdout), flags)
+  }
 });
